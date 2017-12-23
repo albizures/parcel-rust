f0cbf71aaaf61de4575a6efbbd74d154c946bd53
Jose Albizures <albizures3601@gmail.com>
2017-12-22 23:32:46 -0600

    use rust modules

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const getWasmExport = (url, name) => {
+const getWasmExports = (url, names) => {
   return fetch(url)
     .then(response => response.arrayBuffer())
     .then(bytes => WebAssembly.instantiate(bytes, {}))
-    .then(results => results.instance.exports[name])
+    .then(results => names.reduce((obj, name) => {
+      console.log(obj, name)
+      obj[name] = results.instance.exports[name]
+      return obj
+    }, {}))
 };
 
 Promise.all([
-  getWasmExport(require('./add.rs'), 'add'),
-  getWasmExport(require('./substract.wasm'), 'substract')
-]).then(([add, substract]) => {
-  alert(substract(add(1, 2), 3));
+  getWasmExports(require('./main.rs'), ['add', 'multi']),
+  getWasmExports(require('./substract.wasm'), ['substract'])
+]).then((results) => results.reduce((obj, result) => {
+  return Object.assign(obj, result)
+}), {}).then(({add, substract, multi})=> {
+  alert(multi(substract(add(20, 2), 1), 20))
 })
---
+++ utils.rs
@@ -0,0 +1,9 @@
+#[no_mangle]
+pub fn add(a: i32, b: i32) -> i32 {
+    return a + b
+}
+
+#[no_mangle]
+pub fn multi(a: i32, b: i32) -> i32 {
+    return a * b
+}
